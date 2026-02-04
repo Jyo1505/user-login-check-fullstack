e9cb4175b6f663b668d1ccefe21456de40e86663
Jyotsna <merejyotsna@gmail.com>
2026-02-04 13:04:16 +0530

    api

--- backend/controllers/historyController.js
+++ backend/controllers/historyController.js
@@ -1,7 +1,7 @@
 const db = require("../config/db");
 
 exports.getHistory = async (req, res) => {
-  const [rows] = await db.promise().query(
+  const [rows] = await db.query(
     "SELECT * FROM login_history WHERE user_id=? ORDER BY login_time DESC",
     [req.params.userId]
   );
