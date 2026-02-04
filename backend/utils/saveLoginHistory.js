bd2476466968b812f2744c9b687966d7a70ad797
Jyotsna <merejyotsna@gmail.com>
2026-02-04 13:38:10 +0530

    api

--- backend/utils/saveLoginHistory.js
+++ backend/utils/saveLoginHistory.js
@@ -1,7 +1,7 @@
 const db = require("../config/db");
 
 module.exports = async (userId, ip, browser, os, deviceType) => {
-  await db.promise().query(
+  await db.query(
     "INSERT INTO login_history (user_id, ip_address, browser, os, device_type) VALUES (?,?,?,?,?)",
     [userId, ip, browser, os, deviceType]
   );
